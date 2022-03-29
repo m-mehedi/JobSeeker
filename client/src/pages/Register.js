bbe2e4cde53f26f000c4ef58c865173a7e7fbd2b
MEHEDI <me.mehedi@hotmail.com>
2022-03-30 00:54:35 +0600

    Register &Login Page

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Logo, FormRow } from "../components";
+import { Logo, FormRow, Alert } from "../components";
 import Wrapper from "../assets/wrappers/RegisterPage";
 
 const initialState = {
@@ -7,11 +7,17 @@ const initialState = {
   email: "",
   password: "",
   isMember: true,
+  showAlert: false,
 };
 
 const Register = () => {
   const [values, setValues] = useState(initialState);
   // global state and useNavigate
+
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+
   const handleChange = (e) => {
     console.log(e.target);
   };
@@ -25,13 +31,16 @@ const Register = () => {
     <Wrapper className="full-page" onSubmit={onSubmit}>
       <form className="form">
         <Logo />
-        <h3>Login</h3>
-        <FormRow
-          type="text"
-          name="name"
-          value={values.name}
-          handleChange={handleChange}
-        />
+        <h3>{values.isMember ? "Login" : "Register"}</h3>
+        {values.showAlert && <Alert />}
+        {!values.isMember && (
+          <FormRow
+            type="text"
+            name="name"
+            value={values.name}
+            handleChange={handleChange}
+          />
+        )}
         <FormRow
           type="email"
           name="email"
@@ -45,8 +54,15 @@ const Register = () => {
           handleChange={handleChange}
         />
         <button type="submit" className="btn btn-block">
-          Login
+          {!values.isMember?'Register':'Login'}
         </button>
+
+        <p>
+        {values.isMember ? "Not a member yet? " : "Already registered? "}
+          <button type="button" onClick={toggleMember} className="member-btn">
+            {values.isMember ? "Register" : "Login"}
+          </button>
+        </p>
       </form>
     </Wrapper>
   );
