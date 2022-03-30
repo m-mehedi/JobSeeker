cc46d6513f40a364381bacbf85c330fa5d2da84b
MEHEDI <me.mehedi@hotmail.com>
2022-03-30 23:09:18 +0600

    Error handler

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 // const express = require("express");
 import express from 'express'
-import notFoundMiddleware from './middleware/not-found.js';
+import dotenv from 'dotenv'
+dotenv.config()
 const app = express();
+
+import errorHandlerMiddleware from './middleware/error-handler.js';
+import notFoundMiddleware from './middleware/not-found.js';
 
 // middleware
 notFoundMiddleware
 
 app.get("/", (req, res) => {
+    throw new Error('error')
   res.send("Welcome!");
 });
 app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
 const port = process.env.PORT || 5000;
 
