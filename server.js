913faca1a56cb0d2706813b636c382f131ae7106
MEHEDI <me.mehedi@hotmail.com>
2022-03-30 23:33:12 +0600

    Connect to MongoDB

--- server.js
+++ server.js
@@ -1,24 +1,36 @@
 // const express = require("express");
-import express from 'express'
-import dotenv from 'dotenv'
-dotenv.config()
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
 const app = express();
+// MongoDB
+import connectDB from "./db/connect.js";
 
-import errorHandlerMiddleware from './middleware/error-handler.js';
-import notFoundMiddleware from './middleware/not-found.js';
+import errorHandlerMiddleware from "./middleware/error-handler.js";
+import notFoundMiddleware from "./middleware/not-found.js";
 
 // middleware
-notFoundMiddleware
+notFoundMiddleware;
 
 app.get("/", (req, res) => {
-    throw new Error('error')
+  // throw new Error('error')
   res.send("Welcome!");
 });
-app.use(notFoundMiddleware)
-app.use(errorHandlerMiddleware)
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGODB);
+
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  } catch (error) {
+      console.log(error);
+  }
+};
+
+start()
