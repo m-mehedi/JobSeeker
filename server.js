aff8a622c16732028358e6be72f0b89d62c7335b
MEHEDI <me.mehedi@hotmail.com>
2022-03-30 22:54:37 +0600

    backend setup and 404 middleware

---
+++ server.js
@@ -0,0 +1,18 @@
+// const express = require("express");
+import express from 'express'
+import notFoundMiddleware from './middleware/not-found.js';
+const app = express();
+
+// middleware
+notFoundMiddleware
+
+app.get("/", (req, res) => {
+  res.send("Welcome!");
+});
+app.use(notFoundMiddleware)
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
